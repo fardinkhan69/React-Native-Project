1e54e7f26dc29d9ae3443b829cc774a8643aab53
fardinkhan69 <fardinofficial69@gmail.com>
2021-06-03 17:48:27 +0600

    added movie poster , name , fake data separate folder , edited home.js,image.js,assets,icont.js

---
+++ constants/images.js
@@ -0,0 +1,15 @@
+export const narutoimg = require("../assets/images/naruto.jpg");
+export const theMetropolist = require("../assets/images/the_metropolist.jpg");
+export const flashimg = require("../assets/images/flash.jpg");
+export const johnWick = require("../assets/images/John-Wick-2-Poster.jpg");
+export const theTinyDragon = require("../assets/images/the_tiny_dragon.jpg");
+export const underland = require("../assets/images/underland.jpg");
+
+export default {
+    narutoimg,
+    flashimg,
+    johnWick,
+    theMetropolist,
+    theTinyDragon,
+    underland
+}
